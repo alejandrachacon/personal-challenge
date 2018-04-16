1dd33b0fe3820efb32d74265e1a854c13c949dea
Alejandra Chacon <achacon@endava.com>
2018-04-16 11:53:34 -0500

    first commit

---
+++ src/main/java/com/personal/challenge/aledev/AledevApplication.java
@@ -0,0 +1,12 @@
+package com.personal.challenge.aledev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AledevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AledevApplication.class, args);
+	}
+}
